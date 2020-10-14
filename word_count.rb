fd84611dd2bfd65688ae8311d7210141b8c28c14
Michael M <m.migsch@gmail.com>
2020-10-14 21:08:30 +0200

    Update word_count.rb

--- word_count.rb
+++ word_count.rb
@@ -1,19 +1,23 @@
+# Ask the user for the text input.
 puts "Please enter a short text: "
 text = gets.chomp
 words = text.split(" ")
 
-frequencies = Hash.new(0)
+# Create the hash for where the words and freqencies are saved.
+frequencies = Hash.new(0) # Give the values a default value.
 
 words.each do |word|
   frequencies[word] += 1
 end
 
+# Sort the hash by frequencies in descending order.
 frequencies = frequencies.sort_by do |word, freq|
   freq
 end
 
 frequencies.reverse!
 
+# Print out the words and frequencies.
 frequencies.each do |word, freq|
   puts word + " " + freq.to_s
 end
